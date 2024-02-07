9f99f11f32da8fadc02ff51981e3a5d5d167fa08
ikjunchoi <choiikjun1101@gmail.com>
2024-02-07 16:45:37 +0900

    🐛 fix: payload limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.static(join(process.cwd(), '../client/dist/')));
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.enableCors({
     origin: true,
     credentials: true,
