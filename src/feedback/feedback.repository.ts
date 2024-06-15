b13321f735835e9fd60af9c8a9941d675d0ae628
siwonpada <psw041115@gmail.com>
2024-06-16 03:11:00 +0900

    fix: errors

--- src/feedback/feedback.repository.ts
+++ src/feedback/feedback.repository.ts
@@ -1,17 +1,22 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { Feedback, Prisma } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class FeedbackRepository {
+  private readonly logger = new Logger(FeedbackRepository.name);
   constructor(private readonly prismaService: PrismaService) {}
 
   async createFeedback({
     service,
     email,
     feedback,
-    photos,
+    photos = [],
   }: Pick<Feedback, 'service' | 'email' | 'feedback'> & {
     photos?: string[];
   }): Promise<Prisma.FeedbackGetPayload<{ include: { Photos: true } }>> {
@@ -35,6 +40,7 @@ export class FeedbackRepository {
         if (error instanceof PrismaClientKnownRequestError) {
           throw new InternalServerErrorException(error.message);
         }
+        this.logger.error(error);
         throw new InternalServerErrorException(
           'Unexpected server error occurred',
         );
