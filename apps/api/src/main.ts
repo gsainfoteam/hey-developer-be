2567a06ceb4a7261b487267ae00876c53b9c7367
siwonpada <psw041115@gmail.com>
2024-07-08 14:31:53 +0900

    hotfix: cors error

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,22 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   const configService = app.get(ConfigService);
+  // set CORS config
+  const whitelist = [/https:\/\/.*cs.gistory.me/, /http:\/\/localhost:3000/];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.some((regex) => regex.test(origin))) {
+        callback(null, origin);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
+  // swagger auth config
   const config = new DocumentBuilder()
     .setTitle('Hey developer backend API')
     .setDescription('The Hey developer API recipe book')
