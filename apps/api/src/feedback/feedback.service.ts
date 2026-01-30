772a7b4a78c5c05847657ed64f080e5f746977fc
2paperstar <lee@paperst.ar>
2026-01-30 16:33:31 +0900

    feat: send email for feedback

--- apps/api/src/feedback/feedback.service.ts
+++ apps/api/src/feedback/feedback.service.ts
@@ -4,17 +4,35 @@ import { SlackService } from 'nestjs-slack';
 import { ConfigService } from '@nestjs/config';
 import { CreateFeedbackReqDto } from './dto/req/createFeedbackReq.dto';
 import { Feedback } from '@prisma/client';
+import { createTransport, Transporter } from 'nodemailer';
 
 @Injectable()
 export class FeedbackService {
   private readonly logger = new Logger(FeedbackService.name);
   private readonly apiBaseUrl: string;
+  private readonly transporter: Transporter;
   constructor(
     private readonly feedbackRepository: FeedbackRepository,
     private readonly slackService: SlackService,
-    private readonly configSerivce: ConfigService,
+    configService: ConfigService,
   ) {
-    this.apiBaseUrl = this.configSerivce.getOrThrow<string>('api.baseUrl');
+    this.apiBaseUrl = configService.getOrThrow<string>('api.baseUrl');
+    this.transporter = createTransport({
+      host: configService.getOrThrow<string>('EMAIL_HOST'),
+      port: configService.getOrThrow<number>('EMAIL_PORT'),
+      secure: true,
+      auth: {
+        type: 'oauth2',
+        user: configService.getOrThrow<string>('EMAIL_USER'),
+        serviceClient: configService.getOrThrow<string>('EMAIL_SERVICE_CLIENT'),
+        privateKey: configService
+          .getOrThrow<string>('EMAIL_PRIVATE_KEY')
+          .replace(/\\n/g, '\n'),
+        accessUrl: configService.getOrThrow<string>('EMAIL_ACCESS_URL'),
+      },
+      from: `No Reply <${configService.getOrThrow<string>('EMAIL_USER')}>`,
+      tls: { rejectUnauthorized: false },
+    });
   }
 
   async createFeedback({
@@ -31,40 +49,38 @@ export class FeedbackService {
       photos,
     });
 
-    await this.slackService
-      .postMessage({
-        text: '사용자 피드백 접수!',
-        username: 'Hey Developer!',
-        icon_emoji: ':wave:',
-        attachments: [
-          {
-            color: '#2eb886',
-            fields: [
-              {
-                title: '서비스',
-                value: service,
-                short: false,
-              },
-              {
-                title: '피드백 내용',
-                value: feedback,
-                short: false,
-              },
-              {
-                title: '이메일',
-                value: email,
-                short: false,
-              },
-            ],
-          },
-          ...feedbackData.Photos.map((photo) => ({
-            image_url: `${this.apiBaseUrl}/photo/${photo.uuid}`,
-          })),
-        ],
-      })
-      .catch((err) => {
-        this.logger.error(`Failed to send feedback to slack: ${err}`);
-      });
+    const tag = feedbackData.uuid.slice(-4);
+
+    await Promise.allSettled([
+      this.slackService
+        .postMessage({
+          text: `사용자 피드백 접수! #${tag}`,
+          username: 'Hey Developer!',
+          icon_emoji: ':wave:',
+          attachments: [
+            {
+              color: '#2eb886',
+              fields: [
+                { title: '서비스', value: service, short: false },
+                { title: '피드백 내용', value: feedback, short: false },
+                { title: '이메일', value: email, short: false },
+              ],
+            },
+            ...feedbackData.Photos.map((photo) => ({
+              image_url: `${this.apiBaseUrl}/photo/${photo.uuid}`,
+            })),
+          ],
+        })
+        .catch((err) => {
+          this.logger.error(`Failed to send feedback to slack: ${err}`);
+        }),
+      this.transporter.sendMail({
+        to: 'infoteam@gistory.me',
+        replyTo: email || undefined,
+        subject: `문의 요청 (#${tag})`,
+        html: `${feedback} ${photos.map((p) => `<img src="${p}" />`)}`,
+      }),
+    ]);
 
     return feedbackData;
   }
